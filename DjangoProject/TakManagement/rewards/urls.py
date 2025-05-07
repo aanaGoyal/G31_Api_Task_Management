ec8a2b6ab2ce19cdda28854ddcbee87bd592111b
Aana <aanagoyal30@gmail.com>
2025-05-07 20:09:25 +0530

    Done

--- DjangoProject/TakManagement/rewards/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('rewards/', views.reward_page, name='reward_page'),
-    path('feedback/', views.feedback_page, name='feedback_page'),
-]
